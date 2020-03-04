67c57d0397b5e4bb00acd35cc8cb6027291526b9
Goose <sv_gulchak@inbox.ru>
2020-03-04 21:15:40 +0300

    added hosts2

--- my_web_lib/settings.py
+++ my_web_lib/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e-&ij2br&!hplvqm!g=&lo8%^+jeo2k$a@yv_+njgy4!4vi(&5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'http://ramosgoo.pythonanywhere.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'ramosgoo.pythonanywhere.com']
 
 
 # Application definition
