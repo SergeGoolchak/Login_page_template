84e11abdae2c40d598bbae3bb26ae971623e98d0
Goose <sv_gulchak@inbox.ru>
2020-03-05 14:41:19 +0300

    done lab1

--- login/views.py
+++ login/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpResponse
+
 
 class LoginView(View):
+
 	def get(self, request):
 		return render(request, 'login/index.html')
+
+	def post(self, request):
+		html = '<html><body>'
+		for key, value in request.POST.item():
+			html += f'{key}: {value}<br>'
+		html += '</body></html>'
+		return HttpResponse(html)
